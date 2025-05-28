cf4917b3de7321dcfcac0aea4901fc2c3b0d1695
sdzmerve <merve.yetis.15@gmail.com>
2025-05-28 16:04:38 +0200

    Initial commit

---
+++ firebaseConfig.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6LYSdBiQvZbwyVwb4vEEvqB1PgZNqSB8",
+  authDomain: "mahlzeitapp-5604a.firebaseapp.com",
+  projectId: "mahlzeitapp-5604a",
+  storageBucket: "mahlzeitapp-5604a.firebasestorage.app",
+  messagingSenderId: "193732551455",
+  appId: "1:193732551455:web:523aecbb7ddf691768462a",
+  measurementId: "G-ZPS5Q3KJQW"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
