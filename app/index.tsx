ee2515efd891b67d428dbe4ec80641fb47ae96e1
sdzmerve <merve.yetis.15@gmail.com>
2025-06-12 19:25:18 +0200

    keine Fehlermeldung: Basic Simulation funktioniert

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+// app/login/index.tsx
+
+import { View, Text } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>AAAAALLLLLLLOOOOOOO {'\n\n'}
+        desch funktioniert{'\n\n'}
+        Guggst du{'\n\n'}
+        tamam reischt{'\n\n'}
+        nischt mehr guggen diese{'\n\n'}
+        mach keine auge ya
+      </Text>
+    </View>
+  );
+}
